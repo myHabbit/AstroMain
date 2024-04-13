67bf3177301045146c5ecef67a4278789a42d538
Mr wei <2857736333@qq.com>
2024-04-13 15:41:05 +0800

    main

--- src/config.ts
+++ src/config.ts
@@ -26,25 +26,25 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/myHabbit/AstroMain/tree/main?tab=readme-ov-file",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
     name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/myHabbit/AstroMain/tree/main?tab=readme-ov-file",
     linkTitle: `${SITE.title} on Facebook`,
     active: true,
   },
   {
     name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/myHabbit/AstroMain/tree/main?tab=readme-ov-file",
     linkTitle: `${SITE.title} on Instagram`,
     active: true,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/myHabbit/AstroMain/tree/main?tab=readme-ov-file",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
